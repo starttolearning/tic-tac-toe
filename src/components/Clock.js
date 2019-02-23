2b25b618198c135fcaea7b09eab0b743432da81f
Wilton Lee <wilton@Wiltons-MacBook-Pro.local>
2019-02-23 12:05:34 +0800

    Build test and github homepag

---
+++ src/components/Clock.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+export default class Clock extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            date: new Date()
+        }
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        })
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(), 1000
+        );
+
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, World!</h1>
+                <h1>It is now {this.state.date.toLocaleTimeString()}</h1>
+            </div>
+        )
+    }
+}
